955dc5c890c8288daefa8c81301bfec53afada41
Alex Popescu <the.mindstorm@gmail.com>
2013-11-24 23:01:50 -0800

    added space symbols to assertion function

--- tests/assertions.py
+++ tests/assertions.py
@@ -1,11 +1,15 @@
-__author__ = 'alex'
+# -*- coding: utf8 -*-
+
 
 def assertEq(expected, actual):
 #  if len(expected) != len(actual):
 #    raise AssertionError("Length differs: expected %s actual %s" % (len(expected), len(actual)))
-  if expected != actual:
-    for idx in range(min(len(expected), len(actual))):
-      if expected[idx] != actual[idx]:
-        actual = actual[:idx] + ">%s<" % actual[idx] + actual[idx+1:]
-        break
-    raise AssertionError("\nExpected:'''\n%s'''\nActual:'''\n%s'''" % (expected, actual))
+    expected = expected.replace("\t", "....").replace(" ", ".")
+    actual = actual.replace("\t", "....").replace(" ", ".")
+    if expected != actual:
+        for idx in range(min(len(expected), len(actual))):
+            if expected[idx] != actual[idx]:
+                actual = actual[:idx] + ">%s<" % actual[idx] + actual[idx + 1:]
+                break
+        msg = u"\nExpected:'''\n%s'''\nActual:'''\n%s'''" % (expected, actual)
+        raise AssertionError(msg)
