bbc409dc555442ed43eb2ad358076c88478a731a
Alex Popescu <the.mindstorm@gmail.com>
2013-11-24 22:07:06 -0800

    fixed line breaks and hr

--- tests/test_breaks.py
+++ tests/test_breaks.py
@@ -9,12 +9,24 @@ class ParagraphBreaksText(unittest.TestCase):
         in_html = '''<p>This is a paragraph with<br />
 a forced line break<br />
 used in 2 places.'''
-        out_md = '''This is a paragraph with
-a forced line break
+        out_md = u'''This is a paragraph with  
+a forced line break  
 used in 2 places.'''
         assertEq(out_md, html2md.html2md(in_html))
 
 
+    def test_hr(self):
+        in_html = u'''<p>Some text.</p>
+<hr/>
+<p>Follow up text.</p>'''
+        out_md = u'''Some text.
+
+-----
+
+Follow up text.'''
+        assertEq(out_md, html2md.html2md(in_html))
+
 
 if  __name__ == '__main__':
     unittest.main()
+
