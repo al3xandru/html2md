273285c8aa2caab13fff7a2327b0be1065ce6553
Alex Popescu <the.mindstorm@gmail.com>
2013-11-25 16:11:23 -0800

    improved footnotes

--- tests/test_footnotes.py
+++ tests/test_footnotes.py
@@ -24,8 +24,7 @@ class Footnotes(unittest.TestCase):
 <div class="footnotes">
 <hr>
 <ol>
-<li id="fn:1">A nice visualization <a href="http://nosql.mypopescu.com/post/609721873/who-is-using-hbase">here</a>.
-(<a class="footnoteBackLink" href="#fnref:1" rev="footnote" title="Jump back to footnote 1 in the text">h</a>)</li>
+<li id="fn:1">A nice visualization <a href="http://nosql.mypopescu.com/post/609721873/who-is-using-hbase">here</a>.(<a class="footnoteBackLink" href="#fnref:1" rev="footnote" title="Jump back to footnote 1 in the text">h</a>)</li>
 <li id="fn:2">A nice visualization of HBase (<a class="footnoteBackLink" href="#fnref:1" rev="footnote" title="Jump back to footnote 1 in the text">h</a>)</li>
 </ol>
 </div>
